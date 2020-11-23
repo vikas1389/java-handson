a15f102dea89b00d9e94e23175398ff9f5dd4cf6
vikas1389 <vikaskulkarni89@gmail.com>
2020-11-22 22:29:03 -0500

    Converts Integer to Roman

---
+++ intToRoman.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+ 
+ 
+ public static String intToRoman(int num) {
+      String[] thousands = {"", "M", "MM", "MMM"};
+      String[] hundreds = 
+        {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+      String[] tens = 
+        {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+      String[] units = 
+        {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+      return thousands[num / 1000] + 
+           hundreds[(num % 1000) / 100] + 
+           tens[(num % 100) / 10] + 
+           units[num % 10];
+    }
+ 
+ public static void main(String[] args) {
+        
+        int num = 3000;
+        
+       System.out.println("Roman Number is: " +  intToRoman(num));
+        
+   }
+}
